97c19b3e38cee66177bb59b2fdc49f2289253493
vmainente <vinicius.mainente@elsys.com.br>
2021-07-23 09:22:16 -0300

    Adicionado Email e Perfil

--- Gerador de pessoa Vini/Program.cs
+++ Gerador de pessoa Vini/Program.cs
@@ -11,11 +11,16 @@ namespace Gerador_de_pessoa_Vini
             Pessoa vini = new Pessoa()
             {
                 Cpf = new Faker("pt_BR").Person.Cpf(),
-                Nome = new Faker().Person.FullName
+                Nome = new Faker().Person.FullName,
+                Email = new Faker().Person.Email,
+                Perfil = "Loja Geral"
+
             };
 
             Console.WriteLine(vini.Cpf);
             Console.WriteLine(vini.Nome);
+            Console.WriteLine(vini.Email);
+            Console.WriteLine(vini.Perfil);
         }
     }
 }
